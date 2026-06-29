2caa3242b1abf03e39653b31d7f5c269093c8231
Nisha-Sharma676 <nishaasharma3264@gmail.com>
2026-06-30 00:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import os, requests
+import base64
 
 app = Flask(__name__)
 
 VISION_KEY = os.environ.get("VISION_KEY")
 VISION_ENDPOINT = os.environ.get("VISION_ENDPOINT")
 
-URL = VISION_ENDPOINT + "computervision/imageanalysis:analyze?api-version=2024-02-01&features=caption,tags,read,objects"
+URL = VISION_ENDPOINT + "computervision/imageanalysis:analyze?api-version=2024-02-01&features=caption,tags,read,objects,brands"
 
+headers = {
+    "Ocp-Apim-Subscription-Key": VISION_KEY,
+    "Content-Type": "application/json"
+}
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return "Vision AI App Running"
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    try:
-        data = request.json
+    data = request.json
 
-        headers = {
-            "Ocp-Apim-Subscription-Key": VISION_KEY,
-            "Content-Type": "application/json"
-        }
-
+    # CASE 1: URL
+    if "url" in data:
         body = {"url": data["url"]}
 
-        response = requests.post(URL, headers=headers, json=body)
+    # CASE 2: Base64 image
+    elif "image_base64" in data:
+        image_data = data["image_base64"].split(",")[1]
+        body = {"data": image_data}
 
-        return jsonify(response.json())
+    else:
+        return jsonify({"error": "No valid input provided"})
 
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    response = requests.post(URL, headers=headers, json=body)
 
+    return jsonify(response.json())
+
+# Vercel requirement
 app = app
