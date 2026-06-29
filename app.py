9e8a8051b53878aa4d8e2b1189a8889545590e57
Nisha-Sharma676 <nishaasharma3264@gmail.com>
2026-06-30 00:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import os, requests
-import base64
 
 app = Flask(__name__)
 
-VISION_KEY = os.getenv("VISION_KEY")
-VISION_ENDPOINT = os.getenv("VISION_ENDPOINT")
+VISION_KEY = os.environ.get("VISION_KEY")
+VISION_ENDPOINT = os.environ.get("VISION_ENDPOINT")
 
 URL = VISION_ENDPOINT + "computervision/imageanalysis:analyze?api-version=2024-02-01&features=caption,tags,read,objects"
 
@@ -17,26 +16,21 @@ def home():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    data = request.json
+    try:
+        data = request.json
 
-    headers = {
-        "Ocp-Apim-Subscription-Key": VISION_KEY,
-        "Content-Type": "application/octet-stream"
-    }
+        headers = {
+            "Ocp-Apim-Subscription-Key": VISION_KEY,
+            "Content-Type": "application/json"
+        }
 
-    # CASE 1: URL
-    if data.get("url"):
-        body = requests.get(data["url"]).content
+        body = {"url": data["url"]}
 
-    # CASE 2: FILE UPLOAD
-    else:
-        image_data = data["image_base64"].split(",")[1]
-        body = base64.b64decode(image_data)
+        response = requests.post(URL, headers=headers, json=body)
 
-    response = requests.post(URL, headers=headers, data=body)
+        return jsonify(response.json())
 
-    return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app = app
